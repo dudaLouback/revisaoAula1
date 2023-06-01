806c27d6f5c70d991c85d22c3c7b6471164444f3
Maria Eduarda <maria.louback@aluno.cefet-rj.br>
2023-06-01 12:31:07 -0300

    calculadora pronta

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 #include <stdlinb.h>
 
+using namespace;
+
+int calc(int numOne, inte numTwo, int calcOperador, int &aux){
+    int result
+    switch(calcOperador){
+    case '+':
+    result = numOne + numTwo;
+    break;
+    case '-':
+    result = numOne - numTwo;
+    break;
+    case '*':
+    result = numOne * numTwo;
+    break;
+    case '/':
+    result = numOne / numTwo;
+    break;
+    default:
+    aux = 0 ;
+    return 0;
+    }
+    aux = 0;
+    return result;
+}
+
 int main{
 	int num1, num2, ;
 	char operador;
@@ -8,5 +33,8 @@ int main{
 	cin >> num1;
 	cout << "Escolha o segundo número: \n";
 	cin >> num2;
-	cout << "Escolha a operação necessária: ('+' para somar, '-' para subtrair, '*' para multiplicar;) \n";
+	cout << "Escolha a operação necessária: ('+' para somar, '-' para subtrair, '*' para multiplicar, '/' para dividir;) \n";
+	cin >> operador;
+	resultCalc = caul(numOne, numtwo, calcOperador, aux);
+	aux = != 1 ? cout << "Você digitou um número ou operador invalido, tente novamente" : cout << numOne<<calcOperador<<numTwo<<"="<<resultCalc;
 }
