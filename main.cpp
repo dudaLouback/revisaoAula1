5ad04efec2f2eeeef2575a9ed6597145b13bbc4e
Maria Eduarda <maria.louback@aluno.cefet-rj.br>
2023-06-01 11:55:41 -0300

    arquivo1

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlinb.h>
+
+int main{
+	int num1, num2, ;
+	char operador;
+	cout << "Escolha o primeiro número: \n";
+	cin >> num1;
+	cout << "Escolha o segundo número: \n";
+	cin >> num2;
+}
